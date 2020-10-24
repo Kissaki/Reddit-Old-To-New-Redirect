6cb46b4c775a3382fbc598e0762c237bcc482da7
Jan Klass <kissaki@posteo.de>
2020-10-24 14:33:08 +0200

    Drop console.log in onBeforeRequest

--- background_script.js
+++ background_script.js
@@ -2,10 +2,8 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/URL
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
-    console.log(details.url)
     const url = new URL(details.url)
     url.hostname = 'www.reddit.com'
-
     return { redirectUrl: url.href }
   },
   {
